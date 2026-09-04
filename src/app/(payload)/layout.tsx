60f8412d08be2e082f2342a6801cef24c42e9b18
globaltrendarena <updateinformationexpress@gmail.com>
2026-09-04 23:12:56 +0600

    Update layout.tsx

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -4,7 +4,6 @@ import config from '@payload-config'
 import '@payloadcms/next/css'
 import type { ServerFunctionClient } from 'payload'
 import {
-  generatePayloadViewport,
   handleServerFunctions,
   RootLayout,
 } from '@payloadcms/next/layouts'
@@ -13,7 +12,10 @@ import React from 'react'
 import { importMap } from './admin/importMap.js'
 import './custom.css'
 
-export const generateViewport = generatePayloadViewport
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
 
 type Args = {
   children: React.ReactNode
